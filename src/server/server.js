ce993973eede77e8e13de544e2ae77635c463d8e
se0in <tjfls0@naver.com>
2023-12-14 01:26:30 +0900

    Feat : server 가져오기 완료

--- src/server/server.js
+++ src/server/server.js
@@ -4,24 +4,41 @@
 //  * * server 관리
 //  * * 농림축산식품 공공데이터포털 https://data.mafra.go.kr/main.do
 //  * */
-// // server.js
-// import axios from "axios";
+// server.js
+import axios from "axios";
 
 
-// const API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY = process.env.REACT_APP_API_KEY;
+const fetchData = async () => {
+  const url = `openapi/${API_KEY}/xml/Grid_20150827000000000226_1/1/5`;
+  try {
+    const response = await axios.get(url);
 
-// const fetchData = async () => {
-//   try {
-//     const url = `https://211.237.50.150:7080/openapi/${API_KEY}/xml/Grid_20150827000000000226_1/1/5`;
-//     // const url = `/API_KEY=${REACT_APP_API_KEY}&TYPE=json&API_URL=Grid_20150827000000000226_1&START_INDEX=1&END_INDEX5`;
-//     // const {data} = await axios.get('/api'+ url)
-//     const {data} = await axios(url)
-//     console.log('data: ', data);
+    if(response.status === 200) {
+      // * xml parsing
+      const parser = new DOMParser();
+
+      /**
+       * @xmlDoc : 오류 날 경우 console 찍기
+      */
+      const xmlDoc = parser.parseFromString(response.data, 'text/xml');
+      console.log('xmlDoc: ', xmlDoc);
+
+      const extractData = (tagName) => Array.from(xmlDoc.querySelectorAll(tagName)).map((item) => item.textContent);
+      const RECIPE_NM_KO = extractData('RECIPE_NM_KO');
+      console.log('RECIPE_NM_KO: ', RECIPE_NM_KO);
+
+    } else {
+      console.log('데이터 불러오기 실패:', response.status);
+      return null;
+    }
     
-//   } catch (error) {
-//     console.error("에러:", error);
-//     return null;
-//   }
-// };
+  } catch (error) {
+    console.error('에러:', error);
+    return null;
+  }
+};
 
-// export { fetchData };
+export { fetchData };
+
+
